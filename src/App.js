00d18ed2ebe9649268d9feaa2c1307c8aae14654
ankitt01 <ankitrawat0102@gmail.com>
2021-11-12 23:12:49 +0530

    getting started with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
+
+const HatsPage = () => {
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+}
 
 function App() {
     return ( 
       <div>
-        <Homepage />
+        
+        <Routes>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/hats' component={HatsPage} />
+        </Routes>
+        
       </div>
     );
 }
