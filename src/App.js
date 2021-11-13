e13d91e7325eb715dc821ae457fbaadf9e4cb0de
ankitt01 <ankitrawat0102@gmail.com>
2021-11-13 21:03:03 +0530

    react router version change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
-import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
 
 const HatsPage = () => {
+  return(
   <div>
     <h1>Hats Page</h1>
   </div>
+  )
 }
 
 function App() {
     return ( 
       <div>
         
-        <Routes>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/hats' component={HatsPage} />
-        </Routes>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route path='/hats' component={HatsPage} />
+          </Switch>
+        </BrowserRouter>
         
       </div>
     );
